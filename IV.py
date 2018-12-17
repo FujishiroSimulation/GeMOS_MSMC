61fd2b7d4df14e4b637e92fbc0195c3afe8bdbd3
pytk <8115132@ed.tus.ac.jp>
2018-12-17 14:14:34 +0900

    Create IV-Curve

--- IV.py
+++ IV.py
@@ -19,18 +19,18 @@ for voltage in applied_voltages:
     OPTICALSCATTERING ON\n\
     MATERIAL X 0.0 0.2e-6    Y 0.0 16.0e-9  GERMANIUM\n\
     CONDUCTIONBAND PARABOLIC\n\
-    DONORDENSITY    0.       0.         0.2e-6    16.0e-9   1.e23\n\
-    DONORDENSITY    0.       12.0e-9    0.034e-6    16.0e-9   3.e23\n\
-    DONORDENSITY    0.166e-6   12.0e-9    0.2e-6    16.0e-9   3.e23\n\
-    ACCEPTORDENSITY 0.       0.         0.2e-6    16.0e-9   1.e20\n\
+    DONORDENSITY    0.       0.         0.2e-6    16.0e-9   1.e22\n\
+    DONORDENSITY    0.       12.0e-9    0.034e-6    16.0e-9   5.e22\n\
+    DONORDENSITY    0.166e-6   12.0e-9    0.2e-6    16.0e-9   5.e22\n\
+    ACCEPTORDENSITY 0.       0.         0.2e-6    16.0e-9   1.e19\n\
     CONTACT DOWN  0.0    0.2e-6 INSULATOR 0.0\n\
     CONTACT LEFT  0.0    16.0e-9 INSULATOR 0.0\n\
     CONTACT RIGHT 0.0    16.0e-9 INSULATOR 0.0\n\
     CONTACT UP    0.034e-6 0.068e-6 INSULATOR 0.0\n\
     CONTACT UP    0.132e-6 0.166e-6 INSULATOR 0.0\n\
-    CONTACT UP    0.0    0.034e-6 OHMIC     0.0 3.e23\n\
+    CONTACT UP    0.0    0.034e-6 OHMIC     0.0 5.e22\n\
     CONTACT UP    0.068e-6 0.132e-6 SCHOTTKY  -1.0\n\
-    CONTACT UP    0.166e-6 0.2e-6 OHMIC       "+str(voltage)+"  3.e23\n\
+    CONTACT UP    0.166e-6 0.2e-6 OHMIC       "+str(voltage)+"  5.e22\n\
     OXIDE   UP    0.068e-6 0.132-6 0.005e-6 0.3\n\
     OXIDE   DOWN  0.0      0.2e-6  0.010e-6 0.3\n\
     NOQUANTUMEFFECTS\n\
@@ -44,4 +44,4 @@ for voltage in applied_voltages:
     os.system(command)
 
 # remove the script
-os.system("rm-rf script.input")
+os.system("rm -rf script.input")
